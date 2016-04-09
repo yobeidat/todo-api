3846b438e82c404fa7e138c7b6f6f0cd4e195863
nava500 <nava500@outlook.com>
2016-04-10 00:50:07 +0300

    fix port issue

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.get('/',function(req,res){
 	res.send('Todo API Root');
