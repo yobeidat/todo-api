c5f3951eefde185cab2b261ea0eea43cc28e8586
nava500 <nava500@outlook.com>
2016-04-15 17:31:00 +0300

    adding postgres for production

--- server.js
+++ server.js
@@ -33,24 +33,6 @@ app.get('/todos', function(req, res) {
 	}, function(e) {
 		res.status(500).json(e);
 	});
-	/*
-	var filteredTodos = todos;
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		});
-	}
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-		});
-	}
-	res.json(filteredTodos);
-	*/
 });
 
 app.get('/todos/:id', function(req, res) {
@@ -64,16 +46,6 @@ app.get('/todos/:id', function(req, res) {
 	}, function(e) {
 		res.status(500).send();
 	});
-	/*
-	var matchedtodo = _.findWhere(todos, {
-		id: todoitem
-	});
-	if (matchedtodo) {
-		res.json(matchedtodo);
-	} else {
-		res.status(404).send();
-	}
-	*/
 });
 
 app.post('/todos', function(req, res) {
@@ -83,15 +55,6 @@ app.post('/todos', function(req, res) {
 	}, function(e) {
 		res.status(404).json(e);
 	});
-	/*
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		res.status(404).send();
-	}
-	body.id = todoNextId++;
-	body.description = body.description.trim();
-	todos.push(body);
-	res.json(body);
-	*/
 });
 
 app.delete('/todos/:id', function(req, res) {
