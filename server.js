06b1c3a382675cd1f987d13f3a9795b295771611
nava500 <nava500@outlook.com>
2016-04-15 19:17:08 +0300

    updating put request to work with sequelize

--- server.js
+++ server.js
@@ -79,31 +79,29 @@ app.delete('/todos/:id', function(req, res) {
 app.put('/todos/:id', function(req, res) {
 	var body = _.pick(req.body, "description", "completed");
 	var todoitem = parseInt(req.params.id, 10);
-	var matchedtodo = _.findWhere(todos, {
-		id: todoitem
+	var attributes = {};
+
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
+	}
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
+	}
+	db.todo.findById(todoitem).then(function(todo) {
+		if (todo) {
+			todo.update(attributes).then(function(todo) {
+				res.json(todo.toJSON());
+			}, function(e) {
+				res.status(400).json(e);
+			});
+		} else {
+			return res.status(404).send();
+		}
+	}, function() {
+		return res.status(500).send();
 	});
-	var validAttributes = {};
-	if (!matchedtodo) {
-		return res.status(404).json({
-			"error": "no todo found with that id"
-		});
-	}
-
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
-	}
-
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	}
-
-	_.extend(matchedtodo, validAttributes);
-	res.json(matchedtodo);
 });
+
 db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log('server listening to port:' + PORT);
