26fe54ebf8bb68069fba504035a54aaa8d1f4981
nava500 <nava500@outlook.com>
2016-04-15 16:36:59 +0300

    convert get todos to sequelize

--- server.js
+++ server.js
@@ -14,7 +14,26 @@ app.get('/', function(req, res) {
 });
 
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
+	var query = req.query;
+	var where = {};
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
+	}
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
+	}
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
+		res.json(todos);
+	}, function(e) {
+		res.status(500).json(e);
+	});
+	/*
 	var filteredTodos = todos;
 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
 		filteredTodos = _.where(filteredTodos, {
@@ -31,6 +50,7 @@ app.get('/todos', function(req, res) {
 		});
 	}
 	res.json(filteredTodos);
+	*/
 });
 
 app.get('/todos/:id', function(req, res) {
