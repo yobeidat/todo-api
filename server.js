03767888cbaca172afbf3795e331d6bbef031627
nava500 <nava500@outlook.com>
2016-04-16 12:50:18 +0300

    hash the password

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.put('/todos/:id', function(req, res) {
 app.post('/users', function(req, res) {
 	var body = _.pick(req.body, "email", "password");
 	db.user.create(body).then(function(user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function(e) {
 		res.status(404).json(e);
 	});
