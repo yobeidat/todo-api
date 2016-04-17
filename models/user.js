e99aed6cb45f48773644524da3daedb2cc588f16
nava500 <nava500@outlook.com>
2016-04-17 21:17:40 +0300

    adding crypto and user token

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 module.exports = function(sequelize, DataTypes) {
 	var user = sequelize.define('user', {
 		email: {
@@ -66,6 +68,23 @@ module.exports = function(sequelize, DataTypes) {
 				return _.pick(json,
 					"id", "email", "createdAt", "updatedAt"
 				);
+			},
+			generateToken: function(type) {
+				if (!_.isString(type)) {
+					return undefined;
+				}
+				try {
+					var stringData = JSON.stringify({
+						id: this.get('id')
+					}, {type: type});
+					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#').toString();
+					var token = jwt.sign({
+						token: encryptedData
+					},'qwert09');
+					return token;
+				} catch (e) {
+					return undefined;
+				}
 			}
 		}
 	});
